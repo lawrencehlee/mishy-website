ef316847d7df83cd25b7872b829dd410c8fadf9e
Lawrence Lee <lee.lawrenceh@gmail.com>
2023-12-25 01:30:01 -0800

    Setup skeleton and fonts

---
+++ README.md
@@ -0,0 +1,8 @@
+# [mishy.band](https://mishy.band)
+
+## Usage
+
+```shell
+npm install -D tailwindcss
+npx tailwindcss -i src/assets/styles.css -o public/assets/styles.css [--watch]
+```
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/**/*.html"],
+    theme: {
+        extend: {},
+        fontFamily: {
+            "serif": ["Passion One", "serif"],
+            "sans": ["Sarabun", "sans"]
+        }
+    },
+    plugins: [],
+}
+
